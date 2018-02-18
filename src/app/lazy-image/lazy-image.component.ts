220c6666d3a5588c73c871dd316e5171f2a175a0
gonmedtara <gontaramohamed1@gmail.com>
2018-02-18 23:24:49 +0100

    updating

--- src/app/lazy-image/lazy-image.component.ts
+++ src/app/lazy-image/lazy-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-image',
@@ -7,19 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LazyImageComponent implements OnInit {
   public img_background: string = "../../assets/image/cat_20.jpg";
-  public img_full: string = "../../assets/image/cat.jpg";
+  public img_full: string = "https://inst.eecs.berkeley.edu/~cs194-26/fa16/upload/files/proj4/cs194-26-adl/images/cat.jpg";
   public img_src: string = this.img_background;
   public img_class: string = "image-background";
   constructor() { }
 
   ngOnInit() {
-    console.log("this.img_src :", this.img_src);
-    console.log("this.img_class :", this.img_class);
   }
   showFullImage() {
-    console.log("hello show");
     this.img_src = this.img_full;
     this.img_class = "image-full";
-    console.log("hello show end");
   }
 }
